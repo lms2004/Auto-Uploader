221c249d7eb79b109266ef3fa9d16e6cd3153b55
LN <2146289682@qq.com>
2024-05-12 22:30:32 +0800

    0

--- 项目管理系统/Authentication.h
+++ 项目管理系统/Authentication.h
@@ -1,28 +1,31 @@
-#ifndef AUTHENTICATION_SERVICE_H
-#define AUTHENTICATION_SERVICE_H
+//#ifndef AUTHENTICATION_SERVICE_H
+//#define AUTHENTICATION_SERVICE_H
+//
+//#include <iostream>
+//#include <unordered_map>
+//
+////引入使用数据库的库
+//#include <mysql.h>
+//
+////引入用于密码加密的库
+//#include <openssl/evp.h>
+//#include <openssl/rand.h>
+//#include <openssl/sha.h>
+//
+//unsigned char salt[80] = "Gk4^r8@qF0o!Z3Vb$2y$10$V5F9zgYjZLZ1XxqzZ7jJbe47YKjJFJ1YD7k8v1sZ3Zq5u8Nw8eTz7O";
+//
+//class AuthenticationService {
+//public:
+//    AuthenticationService();
+//    ~AuthenticationService();
+//
+//    bool login(const std::string& username, const std::string& password);
+//    bool registerUser(const std::string& username, const std::string& password);
+//
+//private:
+//    std::unordered_map<std::string, std::string> users;
+//    std::string hashPassword(const std::string& password);
+//};
+//
+//#endif // AUTHENTICATION_SERVICE_H
 
-#include <iostream>
-#include <unordered_map>
-
-//引入使用数据库的库
-#include <mysql.h>
-
-//引入用于密码加密的库
-#include <openssl/evp.h>
-#include <openssl/rand.h>
-#include <openssl/sha.h>
-
-class AuthenticationService {
-public:
-    AuthenticationService();
-    ~AuthenticationService();
-
-    bool login(const std::string& username, const std::string& password);
-    bool registerUser(const std::string& username, const std::string& password);
-
-private:
-    std::unordered_map<std::string, std::string> users;
-    std::string hashPassword(const std::string& password);
-};
-
-#endif // AUTHENTICATION_SERVICE_H
--- 项目管理系统/main.cpp
+++ 项目管理系统/main.cpp
@@ -1,12 +1,38 @@
-#include"ProjectManament.h"
-#include"ProjectSorting.h"
-#include"GitHubUploader.h"
-#include"Authentication.h"
+//#include"ProjectManament.h"
+//#include"ProjectSorting.h"
+//#include"GitHubUploader.h"
+//#include"Authentication.h"
+//
+//int main() {
+//    AuthenticationService authService;
+//
+//    std::string username = "1234567890000";
+//    std::string password = "password123";
+//    if (authService.registerUser(username, password)) {
+//        std::cout << "用户注册成功！" << std::endl;
+//    }
+//    else {
+//        std::cout << "用户已存在，注册失败。" << std::endl;
+//    }
+//
+//    std::string loginUsername = "1234567890000";
+//    std::string loginPassword = "password123";
+//    if (authService.login(loginUsername, loginPassword)) {
+//        std::cout << "用户登录成功！" << std::endl;
+//    }
+//    else {
+//        std::cout << "用户名或密码错误，登录失败。" << std::endl;
+//    }
+//
+//    return 0;
+//}
+
+#include "Authentication.h"
 
 int main() {
     AuthenticationService authService;
 
-    std::string username = "l1111";
+    std::string username = "12345678900000";
     std::string password = "password123";
     if (authService.registerUser(username, password)) {
         std::cout << "用户注册成功！" << std::endl;
@@ -15,7 +41,7 @@ int main() {
         std::cout << "用户已存在，注册失败。" << std::endl;
     }
 
-    std::string loginUsername = "l1111";
+    std::string loginUsername = "12345678900000";
     std::string loginPassword = "password123";
     if (authService.login(loginUsername, loginPassword)) {
         std::cout << "用户登录成功！" << std::endl;
@@ -27,12 +53,3 @@ int main() {
     return 0;
 }
 
-//int main()
-//{
-//	AuthenticationService authService;
-//	std::string a=authService.hashPassword("123456");
-//	std::string b = authService.hashPassword("123456");
-//	std::cout << a << "\n";
-//	std::cout << b << "\n";
-//	return 0;
-//}
